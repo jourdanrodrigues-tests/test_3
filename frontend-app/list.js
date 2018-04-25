886d14d2bcc5f22e26ea5daacf82e4d8f79cb5c6
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-04-25 15:04:29 -0300

    :ambulance: Fix SSL difference in images

--- frontend-app/list.js
+++ frontend-app/list.js
@@ -39,7 +39,7 @@
         const content = document.createElement(defaultKeys[key]);
 
         if (key === 'image_url') {
-          content.src = hotel.image_url;
+          content.src = hotel.image_url.replace(/https?:/, '');
           content.style.cssText = 'width: 20em';
         } else if (key === 'amenities') {
           content.innerHTML = hotel.amenities.join('\n');
